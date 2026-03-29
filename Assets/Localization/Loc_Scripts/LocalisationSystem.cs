5d8e20c9cced3e546eae0642c482a16da5a567ce
Farhan <far20shid@gmail.com>
2026-03-29 18:48:05 +0200

    loc update

--- Assets/Localization/Loc_Scripts/LocalisationSystem.cs
+++ Assets/Localization/Loc_Scripts/LocalisationSystem.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LocalizationSystem : MonoBehaviour
@@ -7,24 +8,28 @@ public class LocalizationSystem : MonoBehaviour
     {
         English,
         French,
-        Japanese
+        Japanese,
+        Dutch
     }
 
-    public static Language language = Language.French;
+    public static Language language = Language.English;
 
     public static Dictionary<string, string> localisedEN;
-    public static Dictionary<string, string> localisedFR;
-
+    public static Dictionary<string, string> localisednl;
     public static bool isInit;
-
+    public TextMeshProUGUI txt;
     public static void Init()
     {
         CSVLoader csvLoader = new CSVLoader();
         csvLoader.LoadCSV();
 
         localisedEN = csvLoader.GetDictionaryValues("en");
-        localisedFR = csvLoader.GetDictionaryValues("fr");
+        localisednl = csvLoader.GetDictionaryValues("nl");
 
+        foreach (var v in localisedEN)
+        {
+            Debug.Log(v.Key);
+        }
         isInit = true;
     }
 
@@ -39,11 +44,23 @@ public class LocalizationSystem : MonoBehaviour
             case Language.English:
                 localisedEN.TryGetValue(key, out value);
                 break;
-            case Language.French:
-                localisedFR.TryGetValue(key, out value);
+            case Language.Dutch:
+                localisednl.TryGetValue(key, out value);
                 break;
         }
 
         return value;
+    }
+    public void  OnPressEnglish()
+    {
+        language = Language.English;
+        txt.text = GetLocalisedValue("danny_1");
+    }
+
+    public void OnPressNL()
+    {
+        language = Language.Dutch;
+        txt.text = GetLocalisedValue("danny_1");
+
     }
 }
---
+++ Assets/Localization/Loc_Scripts/LocalizationReplacer.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+//public enum LanguageList
+//{
+//    English,
+//    Japanese,
+//    Korean,
+//    ChineseSimplified,
+//    ChineseTraditional
+
+
+//}
+public class LocalizationReplacer : MonoBehaviour
+{
+    public Image MyImage;
+    public TextMeshProUGUI Mytxt;
+    public TextMeshPro Mytxt2;
+    public string KEY;
+     LocalizationImageSet CurrentImageData;
+    [Header("Regular Image")]
+    public LocalizationImageSet[] localizationDatas;
+    [Space]
+    [Header("Alternate Image")]
+
+    public LocalizationImageSet[] alternateLocalizationDatas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyImage = GetComponent<Image>();
+        Mytxt = GetComponent<TextMeshProUGUI>();
+        Mytxt2 = GetComponent<TextMeshPro>();
+        //call this the first time ever
+        //LocalizationManager.getInstance();
+        // SetData();
+        if (Mytxt != null || Mytxt2 != null)
+        {
+            SetData(KEY);
+        }
+        else
+        {
+            if (MyImage != null && CurrentImageData==null)
+            {
+                SetNewData();
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnValidate()
+    {
+        CurrentImageData = null;
+
+    }
+    void SetData(string KEY)
+    {
+        if (Mytxt != null)
+        {
+
+            Mytxt.text = LocalizationManager.getInstance().getLocalizedString(KEY);
+            //Debug.Log(LocalizationManager.getInstance().getLocalizedString(KEY));
+        }
+        if (Mytxt2 != null)
+        {
+            Mytxt2.text = LocalizationManager.getInstance().getLocalizedString(KEY);
+        }
+
+    }
+     
+   public void SetNewData(bool alternate = false)
+    {
+        //Debug.Log(this.name + " inside setnewdata");
+        if (MyImage == null)
+        {
+            MyImage = GetComponent<Image>();
+        }
+        //var size = LocalizationManager.getInstance().;
+        //localizationDatas = new LocalizationImageSet [size];
+        var dataSet = (alternate ==true) ? alternateLocalizationDatas :localizationDatas ;
+        foreach (var data in dataSet)
+        {
+            if(data.myLanguage != Application.systemLanguage)
+            {
+            }
+            else
+            {
+                CurrentImageData = data;
+                //Debug.Log(this.name + " inside currentimagedata " + CurrentImageData.myLanguage);
+
+                break;
+
+            }
+        }
+        if (CurrentImageData!=null)
+        {
+            if (CurrentImageData.Image != null)
+            {
+                MyImage.sprite = CurrentImageData.Image;
+            }
+        }
+
+    }
+
+    public void ReloadSprite()
+    {
+        if (CurrentImageData != null)
+        {
+            MyImage.sprite = CurrentImageData.Image;
+            Debug.Log("swipe to move " + MyImage.name);
+        }
+    }
+
+    void SetImageData()
+    {
+        int i = 0;
+        var systemlang = Application.systemLanguage;
+        // systemlang = SystemLanguage.Japanese;
+        //if (systemlang == SystemLanguage.Japanese)
+        //{
+        //    i = 1;
+        //}
+
+        if (systemlang == SystemLanguage.ChineseTraditional)
+        {
+            i = 2;
+        }
+
+        else if ((systemlang == SystemLanguage.ChineseSimplified))
+        {
+
+            i = 3;
+
+        }
+        else
+        {
+            i = 0;
+
+        }
+        if (i > localizationDatas.Count())
+        {
+            i = 0;
+        }
+        var data = localizationDatas[i];
+        //logic for image or text
+        if (MyImage != null)
+        {
+
+            SetImage(data);
+        }
+
+        else
+        {
+
+            if (Mytxt != null)
+            {
+                SetForcedText(data);
+            }
+
+        }
+    }
+    void SetImage(LocalizationImageSet data)
+    {
+        if (data.Image != null)
+        {
+            MyImage.sprite = data.Image;
+        }
+    }
+    void SetForcedText(LocalizationImageSet data)
+    {
+
+        if (data.Str != null && data.Str.Length > 0)
+        {
+            Mytxt.text = data.Str;
+        }
+    }
+}
+
+    [System.Serializable]
+    public class LocalizationImageSet
+    {
+
+        public string Name;
+        public SystemLanguage myLanguage;
+        public Sprite Image;
+        public string Str;
+    }
+
